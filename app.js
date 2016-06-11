a6ba73ba786f868f1c605623cc1f33cdc839b02c
tnguyen21 <tbnnumbers@gmail.com>
2016-06-11 15:53:38 -0500

    Separated code in html, js, and css files

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 39.5, lng: -98.35},
     zoom: 8
   });
 }
+var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+var mapOptions = {
+  zoom: 4,
+  center: myLatlng
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+var marker = new google.maps.Marker({
+    position: myLatlng,
+    title:"Hello World!"
+});
+
+// To add the marker to the map, call setMap();
+marker.setMap(map);
